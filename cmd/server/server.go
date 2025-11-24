7834b2de7b41950ad6a0de3a0c64e2305836f7a3
Mathias002 <moussetmathias@gmail.com>
2025-11-24 16:24:46 +0100

    feat: v1 server.go

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,27 +1,24 @@
 package server
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	cmd2 "github.com/axellelanca/urlshortener/cmd"
-	"github.com/axellelanca/urlshortener/internal/api"
-	"github.com/axellelanca/urlshortener/internal/models"
 	"github.com/axellelanca/urlshortener/internal/monitor"
 	"github.com/axellelanca/urlshortener/internal/repository"
 	"github.com/axellelanca/urlshortener/internal/services"
-	"github.com/axellelanca/urlshortener/internal/workers"
-	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
-	"gorm.io/driver/sqlite" // Driver SQLite pour GORM
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	// Driver SQLite pour GORM
 )
+
+var err error
 
 // RunServerCmd représente la commande 'run-server' de Cobra.
 // C'est le point d'entrée pour lancer le serveur de l'application.
@@ -35,16 +32,34 @@ puis lance le serveur HTTP.`,
 		// TODO : créer une variable qui stock la configuration chargée globalement via cmd.cfg
 		// Ne pas oublier la gestion d'erreur et faire un fatalF
 
-		// TODO : Initialiser la connexion à la bBDD
+		cfg := cmd2.Cfg
+		if cfg == nil {
+			log.Fatalf("FATAL: La configuration n'a pas été chargée correctement")
+		}
+
+		// TODO : Initialiser la connexion à la BDD
+		db, dbGormOk := gorm.Open(sqlite.Open(cfg.Database.Name), &gorm.Config{})
+		if dbGormOk != nil {
+			log.Fatalf("FATAL: Échec de la connexion à la base de données: %v", err)
+		}
+
+		sqlDB, dbSqlOk := db.DB()
+		if dbSqlOk != nil {
+			log.Fatalf("FATAL: Échec de l'obtention de la base de données SQL sous-jacente: %v", err)
+		}
 
 		// TODO : Initialiser les repositories.
 		// Créez des instances de GormLinkRepository et GormClickRepository.
+		linkRepo := repository.NewLinkRepository(db)
+		clickRepo := repository.NewClickRepository(db)
 
 		// Laissez le log
 		log.Println("Repositories initialisés.")
 
 		// TODO : Initialiser les services métiers.
 		// Créez des instances de LinkService et ClickService, en leur passant les repositories nécessaires.
+		linkService := services.NewLinkService(linkRepo, clickRepo)
+		ClickService := services.NewClickService(clickRepo)
 
 		// Laissez le log
 		log.Println("Services métiers initialisés.")
